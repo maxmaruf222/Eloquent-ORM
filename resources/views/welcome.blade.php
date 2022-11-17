95c4c296ff54216e290b50a07d1e8fa15e13cc10
maxmaruf222e <maxmaruf222@gmail.comm>
2022-11-18 01:49:47 +0600

    laravel-9 new eloquent_orm_relationships

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>eloquent_orm_relationships</title>
+</head>
+<body>
+    <h1>eloquent_orm_relationships</h1><hr>
+
+    
+</body>
+</html>
