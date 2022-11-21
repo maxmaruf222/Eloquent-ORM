beb66c1cdf0d5a351b5a17ae02cfac4be2d47fcc
maxmaruf222e <maxmaruf222@gmail.comm>
2022-11-21 22:58:13 +0600

    update migration for new environment

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
+
+
     // has one
     public function phone()
     {
--- database/migrations/2022_11_18_143433_create_cars_table.php
+++ database/migrations/2022_11_18_143433_create_cars_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('model');
             $table->unsignedBigInteger('mechanic_id');
+            $table->timestamps();
         });
     }
 
